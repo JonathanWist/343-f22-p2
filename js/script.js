fe080c9cbcb593ed638d582a39ce3cbb09162504
Jonathan Wist <jpwist@live.com>
2022-12-09 18:07:51 -0500

    Cleanup and tidying of code.

--- js/script.js
+++ js/script.js
@@ -67,7 +67,6 @@ async function submitFunc(ev) {
         // Create the HTML elements to be added to the page.
         const resultElems = await createElements(respList, ratings);
         results.append(...resultElems);
-        // results.innerHTML = resultElems.join("");
     }
 }
 
@@ -106,9 +105,12 @@ async function createElements(response, ratings) {
 // @param item: individual search result from YouTube's API
 // @param ratings: rating data for that item
 function createVideoElem(item, ratings) {
+
     let title = item.snippet.title;
     let src = item.snippet.thumbnails.high.url;
     let uploader, viewCount, likes, dislikes;
+
+    // Only assign these values if the current item is a video.
     if (ratings != null) {
         uploader = item.snippet.channelTitle;
         viewCount = ratings.viewCount
@@ -116,15 +118,28 @@ function createVideoElem(item, ratings) {
         dislikes = ratings.dislikes
     }
 
+    // Creates the main card for the item and the body of that card.
     const videoCard = document.createElement("div");
     videoCard.classList.add("card", "text-white", "bg-dark", "mb-3");
+    if (item.id.kind == "youtube#video") {
+        videoCard.classList.add("video");
+        videoCard.id = item.id.videoId;
+    } else {
+        videoCard.classList.add("channel");
+        videoCard.id = item.id.channelId;
+    }
     videoCard.style.maxWidth = "800px";
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
+
+    // Creates the title element, adds it to the card body.
     const cardTitle = document.createElement("h5");
     cardTitle.classList.add("card-title");
     cardTitle.innerHTML = title;
     cardBody.append(cardTitle);
+
+    // Creates the image element, determines the resolution of the image based
+    // on the resolution of the screen, then adds it to the card body.
     const cardImg = document.createElement("img");
     cardImg.classList.add("card-img");
     if (window.matchMedia("(max-width: 319px)").matches) {
@@ -136,45 +151,35 @@ function createVideoElem(item, ratings) {
     }
     cardImg.src = src;
     cardBody.append(cardImg);
+
+    // Creates the elements to be used if the current item is a video.
     const cardUpload = document.createElement("p");
     const cardViews = document.createElement("p");
     const cardLikes = document.createElement("p");
     const cardDislikes = document.createElement("p");
+
+    // Only assigns and appends the previous elements if the current item is a video.
     if (item.id.kind == "youtube#video") {
+        // Video uploader part of the card
         cardUpload.classList.add("card-text");
         cardUpload.innerHTML = `Uploader: ${uploader}`;
         cardBody.append(cardUpload);
+
+        // Video views
         cardViews.classList.add("card-text");
         cardViews.innerHTML = `View Count: ${viewCount}`;
         cardBody.append(cardViews);
+
+        // Video likes
         cardLikes.classList.add("card-text");
         cardLikes.innerHTML = `Likes: ${likes}`;
         cardBody.append(cardLikes);
+
+        // Video dislikes
         cardDislikes.classList.add("card-text");
         cardDislikes.innerHTML = `Dislikes: ${dislikes}`;
         cardBody.append(cardDislikes);
     }
-
-    // let videoCard;
-    // if (item.id.kind == "youtube#video") {
-    //     videoCard = `  <div class="card text-white bg-dark mb-3" style="max-width: 50rem;">
-    //                             <div class="card-body">
-    //                                 <h5 class="card-title">${title}</h5>
-    //                                 <img class="card-img" src="${src}">
-    //                                 <p class="card-text">Uploader: ${uploader}</p>
-    //                                 <p class="card-text">View count: ${viewCount}</p>
-    //                                 <p class="card-text">Likes: ${likes}</p>
-    //                                 <p class="card-text">Dislikes: ${dislikes}</p>
-    //                             </div>
-    //                         </div>`;
-    // } else {
-    //     videoCard = `  <div class="card text-white bg-dark mb-3" style="max-width: 50rem;">
-    //                             <div class="card-body">
-    //                                 <h5 class="card-title">Channel: ${title}</h5>
-    //                                 <img class="card-img" src="${src}">
-    //                             </div>
-    //                         </div>`;
-    // }
     videoCard.append(cardBody);
     return videoCard;
 }
@@ -189,34 +194,26 @@ function clearResultsElem() {
 // Function to alter image sizes for results as the window changes sizes to 
 // dynamically alter them and use the other images of different resolutions.
 function updateImages() {
+    // Only executes if there are images to modify.
     if (respList != null && results.childNodes != null) {
         Array.from(results.childNodes).forEach((item, index) => {
             const img = item.childNodes[0].childNodes[1];
+
+            // Lowest resolution
             if (window.matchMedia("(max-width: 319px)").matches) {
                 img.src = respList[index].snippet.thumbnails.default.url;
             }
+            // Medium-quality resolution
             else if (window.matchMedia("(max-width: 500px)").matches ||
                 (respList[index].id.kind == "youtube#channel" && window.matchMedia("(max-width: 800px)").matches)) {
                 img.src = respList[index].snippet.thumbnails.medium.url;
-            } else {
+            } 
+            // High-quality resolution
+            else {
                 img.src = respList[index].snippet.thumbnails.high.url;
             }
         });
     }
-    // if (respList != null && results.childNodes != null) {
-    //     for (let i = 1; i < results.childNodes.length; i += 2) {
-    //         const img = results.childNodes[i].childNodes[1].childNodes[3];
-            // if (window.matchMedia("(max-width: 319px)").matches) {
-            //     img.src = respList[Math.floor(i/2)].snippet.thumbnails.default.url;
-            // }
-            // else if (window.matchMedia("(max-width: 500px)").matches ||
-            //     (respList[Math.floor(i/2)].id.kind == "youtube#channel" && window.matchMedia("(max-width: 800px)").matches)) {
-            //     img.src = respList[Math.floor(i/2)].snippet.thumbnails.medium.url;
-            // } else {
-            //     img.src = respList[Math.floor(i/2)].snippet.thumbnails.high.url;
-            // }
-    //     }
-    // }
 }
 
 // Function to execute when the 'next page' button is pressed. Fetches
@@ -243,7 +240,6 @@ async function nextFunc() {
         console.log(ratings);
         const resultElems = await createElements(respList, ratings);
         results.append(...resultElems);
-        // results.innerHTML = resultElems.join("");
     }
 }
 
@@ -272,6 +268,5 @@ async function prevFunc() {
         console.log(ratings);
         const resultElems = await createElements(respList, ratings);
         results.append(...resultElems);
-        // results.innerHTML = resultElems.join("");
     }
 }
